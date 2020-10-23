3946b9b8705e0629de2d6cdc34ef2eaae6631458
francesca-giorno <francescagiorno1996@gmail.com>
2020-10-23 15:22:07 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var cognomeUtente = prompt("Qual'è il tuo cognome?");
+var surname = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+//step2
+//surname.push(cognomeUtente);
+
+//arrey arrivo
+var cognomiordinati = [];
+
+fruit.sort();
+ //step 3
+var olEL = document.getElementById("surnameList");
+var i = 0;
+while( i > surname.length){
+
+  i++;
+   olEL.innerHTML += "<li>" + surname[i] + "</li>";
+
+}
